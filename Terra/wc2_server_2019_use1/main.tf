2e804dc5d7a050e357b52f59a5cae634443b1f57
Sudhir Jindal <sudhir.jindal10@gmail.com>
2022-01-22 22:57:48 -0600

    First Commit

---
+++ Terra/wc2_server_2019_use1/main.tf
@@ -0,0 +1,52 @@
+provider "aws" {
+     region = "us-east-1"
+}
+
+variable "availability_zone" {
+     type = string
+     default = "us-east-1a"
+}
+
+#AWS Instance
+
+resource "aws_instance" "example" {
+     ami = data.aws_ami.windows.id
+     instance_type = "t2.micro"
+     availability_zone = var.availability_zone
+
+lifecycle {
+     ignore_changes = [ami]
+     }
+ }
+
+#AMI Filter for Windows Server 2019 Base
+
+data "aws_ami" "windows" {
+     most_recent = true
+
+     filter {
+        name   = "name"
+        values = ["Windows_Server-2019-English-Full-Base-*"]
+
+ }
+
+     filter {
+       name   = "virtualization-type"
+       values = ["hvm"]
+
+ }
+
+     owners = ["801119661308"] # Canonical
+
+ }
+
+ resource "aws_ebs_volume" "example" {
+     availability_zone = var.availability_zone
+     size             = 40
+}
+
+resource "aws_volume_attachment" "ebs_att" {
+     device_name = "/dev/sdh"
+     volume_id   = aws_ebs_volume.example.id
+     instance_id = aws_instance.example.id
+}
